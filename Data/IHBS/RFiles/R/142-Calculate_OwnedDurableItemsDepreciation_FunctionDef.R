5a3af6fe50c22a5fa2e3263fcbb83236d3500130
Kosar-04 <144225849+Kosar-04@users.noreply.github.com>
2025-04-28 00:10:35 +0330

    Update 142-Calculate_OwnedDurableItemsDepreciation_FunctionDef.R

--- Data/IHBS/RFiles/R/142-Calculate_OwnedDurableItemsDepreciation_FunctionDef.R
+++ Data/IHBS/RFiles/R/142-Calculate_OwnedDurableItemsDepreciation_FunctionDef.R
@@ -1,3 +1,10 @@
+# Inputs:
+#   DurableData_ExpDetail: Detailed expenditure data for durable items
+#   DurableItems_OwningDetail: Ownership data for durable items
+#   by: Grouping variables (default: "Item")
+#   Decile: Optional income decile data
+#   DurableItems: Metadata containing depreciation rates
+
 Calculate_OwnedDurableItemsDepreciation <- function(DurableData_ExpDetail,
                                                     DurableItems_OwningDetail,
                                                     by="Item",
@@ -12,6 +19,7 @@ Calculate_OwnedDurableItemsDepreciation <- function(DurableData_ExpDetail,
     by = setdiff(by,"Decile")
     DurableDepr <- data.table(Item=DurableItems$Item)
   }else{
+    # Merge decile information with expenditure and ownership data
     DurableData_ExpDetail <- merge(DurableData_ExpDetail,Decile,by="HHID")
     Ownsm <- merge(Ownsm,Decile,by="HHID")
     
@@ -22,10 +30,12 @@ Calculate_OwnedDurableItemsDepreciation <- function(DurableData_ExpDetail,
   #   DurableItems <- data.table(read_excel(Settings$MetaDataFilePath,
   #                                         sheet=Settings$MDS_DurableItemsDepr))
   # }
-  DurableValues <- DurableData_ExpDetail[,.(Value=mean(Durable_Exp)),by=by]
+  
+  # Calculate average values by grouping variables
   DurableDepr <- merge(DurableDepr,DurableValues,by=by,all.x = TRUE)
   DurableDepr <- merge(DurableDepr,DurableItems,by="Item")
-  
+
+  # Polynomial regression function for decile-based value estimation
   f <- function(X){
     v <- X$Value
     d <- as.integer(as.character(X$Decile))
@@ -35,6 +45,7 @@ Calculate_OwnedDurableItemsDepreciation <- function(DurableData_ExpDetail,
     vp <- predict(mdl,newdata = data.frame(d=d,d2=d2,d3=d3))
     return(vp)
   }
+  # Calculate depreciation values
   if("Decile" %in% by){
     
     DurableDepr <- DurableDepr[order(Item,Decile)]
@@ -51,6 +62,7 @@ Calculate_OwnedDurableItemsDepreciation <- function(DurableData_ExpDetail,
   }else{
     DurableDepr[,DepreciationValue:=Value*Depri/100]
   }
+  # Merge with ownership data and sum by household
   D <- merge(Ownsm,DurableDepr,by=by)
   
   OwnedDurableItemsDepreciation <- D[,.(OwnedDurableItemsDepreciation=sum(.SD$DepreciationValue)),by=HHID]
