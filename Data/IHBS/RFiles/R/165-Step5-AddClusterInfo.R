e694be33bf4cdc7c572fa71a6af0ae56b7fcdcbf
Kosar-04 <144225849+Kosar-04@users.noreply.github.com>
2025-05-01 22:01:16 +0330

    Update 165-Step5-AddClusterInfo.R

--- Data/IHBS/RFiles/R/165-Step5-AddClusterInfo.R
+++ Data/IHBS/RFiles/R/165-Step5-AddClusterInfo.R
@@ -14,12 +14,15 @@ Settings <- yaml.load_file("Settings.yaml")
 library(readxl)
 library(data.table)
 
+# Read the predefined cluster information from the specified Excel sheet into a data.table
 ClusterInfo <- data.table(read_excel(Settings$MetaDataFilePath,sheet=Settings$MDS_GeoX_New))
 
 for(year in (Settings$startyear:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear:",year,"\n"))
+  # Load the previously processed household data (with initial poverty status) for the given year
   load(file=paste0(Settings$HEISProcessedPath,"Y",year,"InitialPoor.rda"))
 
+  # Merge household data (MD) with cluster information by geographical keys
   MD<-merge(MD,ClusterInfo,by=c("NewArea","NewArea_Name","Region"))
   save(MD,file=paste0(Settings$HEISProcessedPath,"Y",year,
                       "InitialPoorClustered.rda"))
