a486af78d71273da12fe2e90ce18e3c155dd76aa
Kosar-04 <144225849+Kosar-04@users.noreply.github.com>
2025-05-05 09:49:38 +0330

    Update 214-Total_Income.R

--- Data/IHBS/RFiles/R/214-Total_Income.R
+++ Data/IHBS/RFiles/R/214-Total_Income.R
@@ -20,7 +20,8 @@ bigdt <- data.table(V1=NA_integer_,N=NA_integer_,year=NA_integer_)[0]
 
 for(year in (85:Settings$endyear)){
   cat(paste0("\n------------------------------\nYear: ",year,"\n"))
-  
+
+   # Load processed income source datasets
    load(file=paste0(Settings$HEISProcessedPath,"Y",year,"HHBase.rda"))
    load(file=paste0(Settings$HEISProcessedPath,"Y",year,"PubWage.rda"))
    load(file=paste0(Settings$HEISProcessedPath,"Y",year,"PrvWages.rda"))
@@ -36,14 +37,16 @@ for(year in (85:Settings$endyear)){
    BussIncomeData[,Sector:=NULL]
    AgriIncomeData[,Sector:=NULL]
    
-   #MyIncome<-merge(HHBase,PubWageData,by =c("HHID"),all=TRUE)
-   
+   # MyIncome<-merge(HHBase,PubWageData,by =c("HHID"),all=TRUE)
+
+   # Merge all income sources step-by-step by HHID
    IncomeTable <- merge(HHBase[,.(HHID)],PubWageData,by="HHID",all.x = TRUE)
    IncomeTable <- merge(IncomeTable,PrvWageData,by="HHID",all.x = TRUE)
    IncomeTable <- merge(IncomeTable,BussIncomeData,by="HHID",all.x = TRUE)
    IncomeTable <- merge(IncomeTable,AgriIncomeData,by="HHID",all.x = TRUE)
    IncomeTable <- merge(IncomeTable,OtherIncomeData,by="HHID",all.x = TRUE)
-   if (year>89) {
+   # Merge subsidy only for years after 1389
+  if (year>89) {
      IncomeTable <- merge(IncomeTable,SubsidyWageData,by="HHID",all.x = TRUE)
    }
    
@@ -58,13 +61,13 @@ for(year in (85:Settings$endyear)){
      IncomeTable[, NetIncome := PubWageNetIncomeY+PrvWageNetIncomeY+BussNetIncomeY+AgriNetIncomeY+OtherIncome]
      
    }
-   
+   # Assign each household a 'WorkClass' based on the dominant income source
    IncomeTable[PubWageNetIncomeY>0 | PubEarners>0, WorkClass:="Pub"]
    IncomeTable[is.na(WorkClass) & PrvWageNetIncomeY >0, WorkClass:="Prv"]
    IncomeTable[is.na(WorkClass) & BussNetIncomeY>0 , WorkClass:="Buss"]
    IncomeTable[is.na(WorkClass) & AgriNetIncomeY>0 , WorkClass:="Agri"]
    IncomeTable[is.na(WorkClass) & OtherIncome>0, WorkClass:="Retr"]
-   
+   # Convert WorkClass column to a categorical variable
    IncomeTable[,WorkClass:=factor(WorkClass)]
 
  
